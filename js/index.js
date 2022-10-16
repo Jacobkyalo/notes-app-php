9f3237d1de7394f0512a409dba73661fb10a5d22
Jacobkyalo <jacobkyalo43@gmail.com>
2022-10-16 18:18:54 +0300

    register form validation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
-let body = document.body;
+const body = document.body;
+const registerForm = document.getElementById("register__form");
+const loginForm = document.getElementById("login__form");
 
 body.style.backgroundColor = "#111";
+
+//listen for register form submit
+registerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //get all input values
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirm-password").value;
+  // errors
+  let usernameError = document.getElementById("username-error");
+  let emailError = document.getElementById("email-error");
+  let passwordError = document.getElementById("password-error");
+  let confirmPasswordError = document.getElementById("confirmpassword-error");
+
+  if (username === "") {
+    usernameError.textContent = "Username is required!";
+  } else if (email === "") {
+    emailError.textContent = "Email is required!";
+  } else if (password === "") {
+    passwordError.textContent = "Password is required!";
+  } else if (confirmPassword === "") {
+    confirmPasswordError.textContent = "ConfirmPassword is required!";
+  } else if (password !== confirmPassword) {
+    confirmPasswordError.textContent = "Passwords do not match!";
+  } else {
+    console.log("Form can be submitted 🥰");
+  }
+});
